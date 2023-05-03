1f0ed1ce09ffc5e661621df96d514d84c64f6301
Khushbu.patel <khushbu.patel@hunterexpress.ca>
2023-05-03 08:59:10 -0400

    first upload

---
+++ RM_Reports/RM_Reports/Controllers/ReportController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RM_Reports.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult MonthlyReport(int vendorId,string month,string issueYear,string vehicleType,bool isShowTotal) {
+            ViewBag.vendorId = vendorId.ToString();
+         ViewBag.month = month;
+            ViewBag.issueYear = issueYear;
+            ViewBag.vehicleType = vehicleType;
+              ViewBag.isShowTotal = isShowTotal;
+            return View();
+        }
+        public ActionResult WorkOrderReport(string poId)
+        {
+            ViewBag.poId = poId;
+            return View();
+        }
+    }
+}
